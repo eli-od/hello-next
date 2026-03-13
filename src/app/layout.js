a60ee82871f1458128711c4a8388bdeec0bc1fe2
endar_li <eli@overdrive.com>
2026-03-13 13:17:52 -0400

    Revert "solution"
    This reverts commit 650728068a9a9a322bb81d210422357862f87280.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,11 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
-  const timestamp = new Date().toLocaleString();
-  console.log('layout rendered');
-  
   return (
     <html lang="en">
       <body>
         {children}
 
-      <footer>Page rendered on {timestamp}</footer>
       </body>
     </html>
   );
